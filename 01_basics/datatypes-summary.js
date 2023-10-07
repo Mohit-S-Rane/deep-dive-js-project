42cd670892ccdd110a8f07a27598f89723ceb59b
Mohit-S-Rane <mohitsrane16@gmail.com>
2023-10-07 11:33:11 +0530

    Memory Stack & Heap

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 // const bigNumber = 3456543576654356754n
 
@@ -32,6 +32,33 @@ const myFunction = function(){
     console.log("Hello world");
 }
 
-console.log(typeof anotherId);
+// console.log(typeof myObj);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+//  Memory 
+
+// Stack(Primitive)  &  Heap(Non Primitive)
+
+let myYoutubename = 'hiteshchoudharydotcom';
+// let anothername = 'chaiaurcode';
+
+let anothername = myYoutubename
+anothername = 'chaiaurcode';
+
+console.table([myYoutubename, anothername]);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne);
+console.log(userTwo);
+
+
