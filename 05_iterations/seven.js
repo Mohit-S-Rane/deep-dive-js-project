98203f42678ca0447592ad815a2a95d1b9a6ff6b
Mohit-S-Rane <mohitsrane16@gmail.com>
2023-10-10 17:31:30 +0530

    DOM

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -2,9 +2,17 @@ const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 // const newNums = myNumers.map( (num) => { return num + 10})
 
-const newNums = myNumers
-                .map((num) => num * 10 )
-                .map( (num) => num + 1)
-                .filter( (num) => num >= 40)
+// const newNums = myNumers
+//                 .map((num) => num * 10 )
+//                 .map( (num) => num + 1)
+//                 .filter( (num) => num >= 40)
 
-console.log(newNums);
+// console.log(newNums);
+
+const numInput = [1,2,3,4,5,6]
+
+const numOutput = numInput.map((num)=>{
+    return (num%2 == 0) ? num*num : num
+})
+
+console.log(numOutput);
